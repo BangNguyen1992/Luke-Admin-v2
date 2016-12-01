b01222ab4732297d0f8707033e7df3e436f5050f
Bang Nguyen <YOUR EMAIL ADDRESS>
2016-12-01 18:16:47 +0200

    Change "get report for admin"

--- components/submission/submissionCtrl.js
+++ components/submission/submissionCtrl.js
@@ -12,65 +12,65 @@
 
 		var vm = this;
 		vm.authService = authService;
-		
+
 		activate();
-		getUser();
+//		getUser();
+
 		function activate() {
-			apiService.get('report')
+			apiService.get('report/admin-get')
 				.then(function (data) {
 					vm.datas = data;
-				console.log(data);
+//					console.log(data);
+
 				});
 		}
-		
-		function getUser(){
-			apiService.get('user/get-all')
-				.then(function (user) {
-					vm.users = user;
-//				console.log("user ID is: " + user.id);
-				});
-		}
-		
-		$scope.delete = function(id) {
-         console.log(id);
-			apiService.get('report/remove?id='+id)
-     };
-		
-		function approve(id) {
-         console.log(id);
-			apiService.get('report/approve?id='+id);
-     };
-		
-		function disapprove(id, state) {
-         console.log(state);
-			apiService.get('report/disapprove?id='+id).then(function(id){
 
-//					state = false;
+//		function getUser() {
+//			apiService.get('user/get-all')
+//				.then(function (user) {
+//					vm.users = user;
+//					//				console.log("user ID is: " + user.id);
+//				});
+//		}
 
+		$scope.delete = function (id) {
+			console.log(id);
+			apiService.get('report/remove?id=' + id).then(function (id) {
+				$state.reload(this);
 			});
-     };
-		
-		$scope.approved = function(id, approved){
-			if(approved){
+		};
+
+		function approve(id) {
+			console.log(id);
+			apiService.get('report/approve?id=' + id).then(function (id) {
+				$state.reload(this);
+			});;
+		};
+
+		function disapprove(id) {
+			console.log(id);
+			apiService.get('report/disapprove?id=' + id).then(function (id) {
+				$state.reload(this);
+			});
+		};
+
+		$scope.approved = function (id, approved) {
+			if (approved)
 				disapprove(id, approved);
-			} else {
+			else if(!approved)
 				approve(id);
-			}
+//			else
+
 		}
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
 		$scope.saveUser = function (event) {
-            event.preventDefault();
-        };
+			event.preventDefault();
+		};
 		$scope.sortType = ' '; // set the default sort type
-    $scope.sortReverse = false; // set the default sort order
-		
-		
+		$scope.sortReverse = false; // set the default sort order
+
+
 	}
 }());
